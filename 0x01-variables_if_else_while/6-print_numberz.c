8bbfc5e5901da52ff4cdd82aefb272158a7384bb
Bn-Bushra <aybuhabi@gmail.com>
2022-09-12 20:46:23 -0700

    a program that prints all single digit numbers of base 10 starting from 0, followed by a new line

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,17 +3,17 @@
 
 /**
 	* main - entry point of the programme
-	* 
 	* Takes no argument
 	*
-	* Description: 
+	* Description: a program that prints the lowercase
+	* alphabet in reverse, followed by a new line.
 	*
 	* Return: Programme always returns (0)
 */
 int main(void)
 {
-	int i; 	
-	
+	int i;
+
 	for (i = 48; 8 <= 57; i++)
 	{
 		putchar(i);

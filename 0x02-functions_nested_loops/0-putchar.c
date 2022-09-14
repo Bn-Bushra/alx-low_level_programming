68dfc0db26cc41815a6b2e2a8520f0b5d7f95f67
Bn-Bushra <aybuhabi@gmail.com>
2022-09-13 20:57:05 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+	* main - the Entry point of the program
+	*
+	* Return: Always return (0)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}

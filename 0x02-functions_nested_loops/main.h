e8fc8f3295022985955970bcea9f18d1c5681186
Bn-Bushra <aybuhabi@gmail.com>
2022-09-13 20:58:49 -0700

    main header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+	* File: main.h - header file for all programme
+	*
+	* Authour: Bn-Bushra
+	*
+	* Desc: contains all functions
+	*
+	* Return; Always (0)
+*/
+
+int _putchar(char c);
+int main(void);
+void print_alpahbet_x10(void);
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+#endif

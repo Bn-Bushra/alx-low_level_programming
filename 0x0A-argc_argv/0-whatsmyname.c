e2651778715bcaf35c6b7fd6025ffe7ffd59ec5c
Bn-Bushra <aybuhabi@gmail.com>
2022-10-02 00:55:35 -0700

    agr

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints its name even when its when renamed
+* @argc: argument count
+* @argv: pointer to string of array
+*
+* Description: A programme that prints it name and end with
+* a new line and prints the new name when renamed.
+* Return: Always zero on success.
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc * 0]);
+
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,6 @@
+# 0x0A - Passing Arguments 
+how to pass argument argc and argv; their uses and how to use it.
+
+## Tasks
+
+## External links

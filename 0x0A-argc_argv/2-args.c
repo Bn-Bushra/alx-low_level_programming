40d0fbe1522c803716501c7cac585e4f9f2b5906
Bn-Bushra <aybuhabi@gmail.com>
2022-10-05 07:00:51 -0700

    one for all

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the programme name and arguments passed
+ * @argc: argument count
+ * @argv: name of argument passed
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	return (0);
+}

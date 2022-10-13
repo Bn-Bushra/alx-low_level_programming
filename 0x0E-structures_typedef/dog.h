8b1d188e94789b55f016f5735e0c6dc0636ea484
Bn-Bushra <aybuhabi@gmail.com>
2022-10-12 21:25:35 -0700

    woof! woof!

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/**
+	* struct dog - has the information about a dog
+	* @name: the dog name
+	* @age: its age
+	* @owner: the name of its owner
+	*/
+struct dog { char *name; float age; char *owner;};

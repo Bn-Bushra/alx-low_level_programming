318d178f26d5d3042d355cc29ae33babfca8c5a5
Bn-Bushra <aybuhabi@gmail.com>
2022-10-14 21:16:04 +0100

    another one

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,23 @@
-#include "variadic_functions.h"
-#include <stdarg.h>
-
-/**
-* sum_them_all - this fucntionreturns the sum of all parameters
-* @an: the guard
-* Return: the sum
-*/
-int sum_them_all(const unsigned in n, ...)
-{
-	unsigned int i;
-	int sum;
-	va_list argz;
-
-	sum - 0;
-	if (!n)
-	return (0);
-	va_start(argz, n);
-	for (i = 0; i < n; i++)
-	{
-			sum = sum + va_arg(argz, int);
-	va_end(argz);
-	return (sum);
+#include "variadic_functions.h"                                                                       
+#include <stdarg.h>                                                                                   
+/**                                                                                                   
+* sum_them_all - this fucntionreturns the sum of all parameters                                       
+* @an: the guard                                                                                      
+* Return: the sum of the list
+*/                                                                                                    
+int sum_them_all(const unsigned in n, ...)                                                            
+{                                                                                                     
+        unsigned int i;                                                                               
+        int sum;                                                                                      
+        va_list argz;                                                                                 
+                                                                                                      
+        sum - 0;                                                                                      
+        if (!n)                                                                                       
+        return (0);                                                                                   
+        va_start(argz, n);                                                                            
+        for (i = 0; i < n; i++)                                                                       
+        {                                                                                             
+                        sum = sum + va_arg(argz, int);                                                
+        va_end(argz);                                                                                 
+        return (sum);                                                                                 
 }
